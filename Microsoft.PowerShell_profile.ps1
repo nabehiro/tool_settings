0dc72fa85402a4eba747ce7ed50f330f12e1427b
Hiroyuki Watanabe <v_hirow@sigmact.com>
2016-01-11 01:38:17 +0900

    change color

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,11 @@
-
+﻿
 # Load posh-git example profile
 . 'C:\tools\posh-git\profile.example.ps1'
 
 # change prompt color
 function prompt
 {
-  Write-Host "@" -NoNewLine -ForegroundColor red
-  Write-Host "$(Convert-Path $(get-location))>" -nonewline -foregroundcolor Magenta
+  Write-Host "@" -NoNewLine -ForegroundColor yellow
+  Write-Host "$(Convert-Path $(get-location))>" -nonewline -foregroundcolor blue
   return ' '
 }
