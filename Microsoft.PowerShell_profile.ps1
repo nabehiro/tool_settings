aba01bf26a4752e3665fbb7c9fa6e9db519ea2ed
Hiroyuki Watanabe <v_hirow@sigmact.com>
2018-11-29 12:15:30 +0900

    posh-git の設定値で prompt を調整するように変更

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,24 +1,7 @@
-
-################################################################################
-# Reference to posh-git.example 'C:\tools\posh-git\profile.example.ps1'
-################################################################################
+# https://github.com/dahlbyk/posh-git
 
 # Load posh-git module 
 Import-Module posh-git
 
-# If module is installed in a default location ($env:PSModulePath),
-# use this instead (see about_Modules for more information):
-# Import-Module posh-git
-
-
-# Set up a simple prompt, adding the git prompt parts inside git repos
-function global:prompt {
-    Write-Host "@ " -NoNewLine -ForegroundColor darkred
-    Write-Host $pwd.ProviderPath.Replace($HOME, "~") -nonewline -foregroundcolor blue
-    Write-VcsStatus
-    Write-Host ""
-    Write-Host ">" -NoNewLine -foregroundcolor blue
-    return " "
-}
-
-Start-SshAgent -Quiet
+$GitPromptSettings.DefaultPromptPath.ForegroundColor = 0x8A2BE2
+$GitPromptSettings.DefaultPromptSuffix.ForegroundColor = 0xADD8E6
