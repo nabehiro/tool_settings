1d6d1d9ccc46dcd42e704124e17805920cfdb6b1
Hiroyuki Watanabe <v_hirow@sigmact.com>
2019-06-15 17:30:24 +0900

    anaconda の prompt "(base)PS>" になるので、明示的にposh-git を呼ぶ

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -5,3 +5,10 @@ Import-Module posh-git
 
 $GitPromptSettings.DefaultPromptPath.ForegroundColor = 0x8A2BE2
 $GitPromptSettings.DefaultPromptSuffix.ForegroundColor = 0xADD8E6
+
+function prompt {
+    # Your non-prompt logic here
+
+    # Have posh-git display its default prompt
+    & $GitPromptScriptBlock
+}
