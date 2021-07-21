7d71df50ada3e4c6b191fd7bcde6df613f56f055
Hiroyuki Watanabe <v_hirow@sigmact.com>
2021-07-21 18:33:35 +0900

    Update del_old.sh

--- pi/radio/del_old.sh
+++ pi/radio/del_old.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-#cd /mnt/hdd1/shared/radio
 cd /home/pi/shared/radio
+
+if test -n "`ls -t . | tail -n+12`";then
+	rm -v `ls -t . | tail -n+12`
+fi
+
+
+cd /home/pi/shared/radio2
 
 if test -n "`ls -t . | tail -n+12`";then
 	rm -v `ls -t . | tail -n+12`
