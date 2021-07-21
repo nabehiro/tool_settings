8ab7551074be62f9ddee5eec287d9f1a55524f76
Hiroyuki Watanabe <v_hirow@sigmact.com>
2021-07-21 18:50:49 +0900

    Update del_old.sh

--- pi/radio/del_old.sh
+++ pi/radio/del_old.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
+# delete old files of "Basic English 3"
 cd /home/pi/shared/radio
 
 if test -n "`ls -t . | tail -n+12`";then
 	rm -v `ls -t . | tail -n+12`
 fi
 
-
+# delete old files of "Enjoy Simple English"
 cd /home/pi/shared/radio2
 
 if test -n "`ls -t . | tail -n+12`";then
